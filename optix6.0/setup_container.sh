8b8d7285a9299309b5f53db659dd09eb2c0f4451
Sam Eriksen <sam.eriksen@bristol.ac.uk>
2021-03-05 13:52:13 +0000

    Add container setup for optix 6.0

---
+++ optix6.0/setup_container.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+container-name(){
+  echo optix
+}
+
+build-optix() {
+
+  docker build -t $(container-name) .
+}
+
+
+run-optix() {
+#centos_search_loc=/usr/lib64/*
+search_loc=/usr/lib64/*
+optix_libs=$(find ${search_loc} | grep -i optix)
+rt_libs=$(find ${search_loc} | grep -i rtcore)
+
+to_mount=""
+
+for vol in $optix_libs $rt_libs
+do
+to_mount="$to_mount -v ${vol}:${vol}"
+done
+
+echo "Starting optix container with 1 GPU"
+
+docker run ${to_mount} --rm --runtime=nvidia -it --gpus 1 $(container-name) /bin/bash
+}
