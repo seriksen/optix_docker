253173cc56353211be1386134e9b251aaefa0b5f
Sam Eriksen <sam.eriksen@bristol.ac.uk>
2021-03-05 17:17:54 +0000

    Try mount swrast

--- optix6.0/setup_container.sh
+++ optix6.0/setup_container.sh
@@ -15,10 +15,10 @@ run-optix() {
 search_loc=/usr/lib64/*
 optix_libs=$(find ${search_loc} | grep -i optix)
 rt_libs=$(find ${search_loc} | grep -i rtcore)
-
+swrast_dri=$(find ${search_loc} | grep -i swrast )
 to_mount=""
 
-for vol in $optix_libs $rt_libs
+for vol in $optix_libs $rt_libs $swrast_dri
 do
 to_mount="$to_mount -v ${vol}:${vol}"
 done
