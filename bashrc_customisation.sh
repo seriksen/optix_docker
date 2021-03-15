d5e39e98014a9be11f10951afe88a8b6507709f1
Sam Eriksen <sam.eriksen@bristol.ac.uk>
2021-03-15 14:30:00 +0000

    Add bashrc_customisation.sh

---
+++ bashrc_customisation.sh
@@ -0,0 +1,20 @@
+export PS1='\[\033[02;32m\]sam@\H-container:\[\033[02;34m\]\w\$\[\033[00m\] '
+
+alias ls='ls --color=auto'
+alias vim='vi'
+alias la='ls -a --color=auto'
+alias ll='la -l -h --color=auto'
+alias ..='cd ..; ls'
+
+start_desktop() {
+  /opt/TurboVNC/bin/vncserver -vgl -wm xfce4-session
+}
+
+kill_desktop() {
+  if [ $# -eq 0 ]
+  then
+    echo "Provide the DISPLAY. eg :25"
+  else
+    /opt/TurboVNC/bin/vncserver -kill $1
+  fi
+}
