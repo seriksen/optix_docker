1967508882022bed7af0cace35469f70888ddb1e
Sam Eriksen <sam.eriksen@bristol.ac.uk>
2021-03-16 11:05:00 +0000

    optix_docker name change

--- bashrc_customisation.sh
+++ bashrc_customisation.sh
@@ -1,4 +1,4 @@
-export PS1='\[\033[02;32m\]\u@optix_desktop:\[\033[02;34m\]\w\$\[\033[00m\] '
+export PS1='\[\033[02;32m\]\u@optix_docker:\[\033[02;34m\]\w\$\[\033[00m\] '
 
 export LD_LIBRARY_PATH=/usr/lib64:/usr/lib:${LD_LIRARY_PATH}
 
